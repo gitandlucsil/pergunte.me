f25e31b7c1a7d736784e44f8c82a7b2441b1ae77
andre <andlucsil1221@gmail.com>
2018-12-17 21:39:21 -0200

    alterado post no servidor para salvar usuario

--- Node/microblog/usuario/usuario.ws.js
+++ Node/microblog/usuario/usuario.ws.js
@@ -1,7 +1,18 @@
 const dao = require('./usuario.dao')
+const multer = require('multer')
+const fs = require('fs')
 
 module.exports = (app) => {
 
+    const configMulter = multer.diskStorage(
+        {
+            destination : (req, res, cb) => {
+                cb(null, './uploads')
+            }, filename : (req, file, cb) => {
+                cb(null,file.originalname)
+            }
+        }
+    )
     app.route("/usuario/login").post( (req, resp) => {
         dao.consultar(req.body, (result) => {
             resp.json(result)
@@ -10,8 +21,25 @@ module.exports = (app) => {
 
     app.route("/usuario/salvar").post( (req, resp) => {
         dao.salvar(req.body, () => {
-            resp.end()
+            let img = req.body.imagem.split(';base64,').pop()
+            fs.writeFile('./uploads/imagem.png',img,{encondingg : 'base64'}, (e) => {
+                res.end()
+            })
         })
     })
+
+    /*app.post('/upload', multer({storage : configMulter}).single('arquivo'),(req, res) => {
+        let obj = JSON.parse(req.body.dados)
+        console.log(obj)
+        res.end()
+    })*/
+
+
+    /*app.post('/upload64',multer().none(), (req, res) => {
+        let img = req.body.imagem.split(';base64,').pop()
+        fs.writeFile('./uploads/imagem.png',img,{encondingg : 'base64'}, (e) => {
+            res.end()
+        })
+    })*/
 
 }
