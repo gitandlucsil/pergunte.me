6dde307a0bb65f2b85ab7b1ed7d5e56d1497ec27
andre <andlucsil1221@gmail.com>
2018-12-20 23:48:31 -0200

    inserido o logout do usuario do sistema

--- Angular/microblog/src/app/home/home.component.ts
+++ Angular/microblog/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient, private auth : AuthService, private router : Router) { }
 
   ngOnInit() {
+  }
+
+  sair(){
+    this.auth.logout()
+    this.router.navigateByUrl('/login')
   }
 
 }
